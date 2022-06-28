9d9ad56c9879de27cc3665ea0b42464e408ecc0d
Riccardo Sangiorgio <richard.sangiorgio@gmail.com>
2022-06-29 00:49:34 +0200

    Dati utente loggato.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,10 @@
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
-  
+
   private
 
   def respond_with(resource, _opts = {})
-    render json: { message: 'Logged in.' }, status: :ok
+    render json: { data: JSON.parse(@user.to_json), message: 'Logged in.' }, status: :ok
   end
 
   def respond_to_on_destroy
