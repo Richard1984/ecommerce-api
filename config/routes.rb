4d3db6f13e2120b701b7b7dd7f265d19da31affa
Riccardo Sangiorgio <richard.sangiorgio@gmail.com>
2022-06-29 02:51:25 +0200

    AccountController (va bene? boh).

--- config/routes.rb
+++ config/routes.rb
@@ -31,4 +31,8 @@ Rails.application.routes.draw do
   resources :categories do
     resources :products, only: [:index]
   end
+
+  resources :account do
+    resources :account, only: [:index]
+  end
 end
