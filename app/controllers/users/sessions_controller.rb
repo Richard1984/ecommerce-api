1c4625a409c5d7f703eea629513c46db538350cc
Riccardo Mancini <riccardo.mancini01@gmail.com>
2022-07-01 00:54:12 +0200

    I problemi login/logout non esistevano

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,19 +4,18 @@ class Users::SessionsController < Devise::SessionsController
   private
 
   def respond_with(resource, _opts = {})
-    # Da ricontrollare, il login sembra andare a buon fine, ma le altre funzioni sembrano non ricordarsi dell'utente (mentre dopo il signup se ne ricordano)
     render json: { data: @user, message: 'Logged in.' }, status: :ok
   end
 
   def respond_to_on_destroy
-    current_user ? log_out_failure : log_out_success
+    current_user ? log_out_success : log_out_failure
   end
 
   def log_out_success
-    render json: { message: "Logged out." }, status: :ok
+    render json: { message: "Logged out.", data: current_user }, status: :ok
   end
 
   def log_out_failure
-    render json: { message: "Logged out failure.", data: current_user }, status: :unauthorized
+    render json: { message: "Couldn't find an active session." }, status: :unauthorized
   end
 end
