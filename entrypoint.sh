a8cf7c5e3f944bbe91587d728d1f62a9a200f2bc
Riccardo Sangiorgio <richard.sangiorgio@gmail.com>
2022-07-14 16:39:38 +0200

    master.key Docker.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /usr/src/app/tmp/pids/server.pid
 
+mkdir -p /usr/src/app/config/
+touch /usr/src/app/config/master.key
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
