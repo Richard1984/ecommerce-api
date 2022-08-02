f2a207c8ecca6a4e203c9681c29337b61af3316b
Riccardo Mancini <riccardo.mancini01@gmail.com>
2022-08-02 18:05:02 +0200

    Aggiunti scenari a rspec search_product

--- spec/requests/product_spec.rb
+++ spec/requests/product_spec.rb
@@ -5,14 +5,26 @@ RSpec.describe "Products", type: :request do
 
     setup do
       @keyboards = ["Keyboard RGB", "Cheap keyboard", "MechanicalKeyboard"]
-      non_keyboards = ["Cabbage"]
+      @non_keyboards = ["Cabbage"]
 
       @keyboards.each do |name|
         Product.create!(name: name, availability: 10, price: "1.48", description: "Description", category_id: nil, available: true)
       end
 
-      non_keyboards.each do |name|
+      @non_keyboards.each do |name|
         Product.create!(name: name, availability: 10, price: "1.48", description: "Description", category_id: nil, available: true)
+      end
+    end
+
+    context  "given no search term or sorting criteria" do
+      it "returns all products" do
+        get "/products"
+        
+        expect(response).to have_http_status(:ok)
+        expect(response.content_type).to eq("application/json; charset=utf-8")
+        
+        response_json = JSON.parse(response.body)
+        expect(response_json["data"].length).to eq(@keyboards.length + @non_keyboards.length)
       end
     end
 
@@ -28,6 +40,26 @@ RSpec.describe "Products", type: :request do
 
         response_json["data"].each do |product|
           expect(product["name"].downcase).to include("keyboard")
+        end
+      end
+    end
+
+    context  "given sorting criteria 'name' and sorting order 'desc'" do
+      it "returns all products sorted by name in descending order" do
+        get "/products?sort_criteria=name&sort_order=desc"
+        
+        expect(response).to have_http_status(:ok)
+        expect(response.content_type).to eq("application/json; charset=utf-8")
+        
+        response_json = JSON.parse(response.body)
+        expect(response_json["data"].length).to eq(@keyboards.length + @non_keyboards.length)
+
+        previous_name = response_json["data"][0]["name"].downcase
+        response_json["data"].each do |product|
+          puts "#{previous_name} > #{product["name"].downcase}"
+          expect(product["name"].downcase).to be <= previous_name
+
+          previous_name = product["name"].downcase
         end
       end
     end
